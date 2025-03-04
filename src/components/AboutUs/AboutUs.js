7e72269332be5f6a44a84e503d6d45e71ddd5ed6
sazzad tomal <sazzadtomal1@gmail.com>
2025-03-04 20:11:09 +0600

    add contant in aboutUs section

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./AboutUs.css";
+import aboutUsPhoto from "../../accets/aboutUsPhoto/aboutUsPhoto.png";
 
 const AboutUs = () => {
   return (
     <div className="about_us_parent_div">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat modi
-        dolores eligendi exercitationem incidunt earum.
-      </h1>
+      <h2>Why Choose QuickVanUK?</h2>
+      <div className="about_us_flex_div">
+        <div className="about_us_text_div">
+          <p>
+            We are a tech-driven, customer-focused van and logistics service
+            offering reliable, affordable, and efficient transportation.
+          </p>
+          <p>
+            🔹Mission & Values – Reliability, Affordability, Efficiency.🔹 Meet
+            Our Team – Professional drivers, support staff, and logistics
+            experts.
+          </p>
+        </div>
+        <img src={aboutUsPhoto} alt="" />
+      </div>
     </div>
   );
 };
--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Pricing.css";
 
+import pricingPhoto from "../../accets/pricing/pricingPhoto.jpg";
+
 const Pricing = () => {
   return (
     <div className="pricing_parent_div">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at
-        eligendi ipsa distinctio commodi recusandae odio, laborum architecto
-        nesciunt veritatis!
-      </h1>
+      <h2>Transparent & Competitive Rates</h2>
+      <div className="pricing_flex_div">
+        <img src={pricingPhoto} alt="" />
+        <div className="pricing_text_div">
+          <p>Hourly Rates – Flexible pricing based on service duration.</p>
+          <p>Fixed Rates – Pre-determined pricing for specific routes.</p>
+          <p>Extra Charges – Stairs, wait time, or long-distance fees apply.</p>
+          <p>
+            Discounts & Promotions – Special rates for returning customers and
+            bulk bookings.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
