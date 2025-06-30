07deb5b45f0e7fc6945c6e9da8b230972a63b449
sazzad tomal <sazzadtomal1@gmail.com>
2025-06-30 19:51:15 +0600

    add photo in book a van section

--- src/components/BookAVan/BookAVan.js
+++ src/components/BookAVan/BookAVan.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./BookAVan.css";
 import emailjs from "@emailjs/browser";
+import bookAVan from "../../accets/bookAVanPhoto/book a van.png";
 
 const BookAVan = () => {
   const form = useRef();
@@ -29,79 +30,84 @@ const BookAVan = () => {
   // For Chrome, Firefox, IE and Opera
   document.documentElement.scrollTop = 0;
   return (
-    <div className="book_a_van_parent_div">
-      <div className="book_a_van_form_div">
-        <h1>Book A Van</h1>
-        <form ref={form} onSubmit={sendEmail}>
-          <div className="form_control">
-            <label className="label">
-              <span className="label-text">Your Name</span>
-            </label>
-            <label className="input_group">
-              <input
-                type="text"
-                name="from_name"
-                placeholder="Your Name"
-                className="input_field"
-                required
-              />
-            </label>
-          </div>
-          <div className="form_control">
-            <label className="label">
-              <span className="label-text">Your Email</span>
-            </label>
-            <label className="input_group">
-              <input
-                type="email"
-                name="from_email"
-                placeholder="Enter Your Email"
-                className="input_field"
-                required
-              />
-            </label>
-          </div>
-          <div className="form_control">
-            <label className="label">
-              <span className="label-text">Phone Number</span>
-            </label>
-            <label className="input_group">
-              <input
-                type="number"
-                name="from_number"
-                placeholder="Enter Your Phone Number"
-                className="input_field"
-                required
-              />
-            </label>
-          </div>
-          <div className="form_control">
-            <label className="label">
-              <span className="label-text">Physical Address</span>
-            </label>
-            <label className="input_group">
-              <input
-                type="text"
-                name="from_address"
-                placeholder="Enter Your Physical Address"
-                className="input_field"
-                required
-              />
-            </label>
-          </div>
-          <textarea className="text_area" name="message"></textarea>
+    <div className="book_a_van_parent_grid_div">
+      <div className="bookAVan_photo_div">
+        <img className="bookAVan_photo" src={bookAVan} alt="" />
+      </div>
+      <div className="book_a_van_parent_div">
+        <div className="book_a_van_form_div">
+          <h1>Book A Van</h1>
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="form_control">
+              <label className="label">
+                <span className="label-text">Your Name</span>
+              </label>
+              <label className="input_group">
+                <input
+                  type="text"
+                  name="from_name"
+                  placeholder="Your Name"
+                  className="input_field"
+                  required
+                />
+              </label>
+            </div>
+            <div className="form_control">
+              <label className="label">
+                <span className="label-text">Your Email</span>
+              </label>
+              <label className="input_group">
+                <input
+                  type="email"
+                  name="from_email"
+                  placeholder="Enter Your Email"
+                  className="input_field"
+                  required
+                />
+              </label>
+            </div>
+            <div className="form_control">
+              <label className="label">
+                <span className="label-text">Phone Number</span>
+              </label>
+              <label className="input_group">
+                <input
+                  type="number"
+                  name="from_number"
+                  placeholder="Enter Your Phone Number"
+                  className="input_field"
+                  required
+                />
+              </label>
+            </div>
+            <div className="form_control">
+              <label className="label">
+                <span className="label-text">Physical Address</span>
+              </label>
+              <label className="input_group">
+                <input
+                  type="text"
+                  name="from_address"
+                  placeholder="Enter Your Physical Address"
+                  className="input_field"
+                  required
+                />
+              </label>
+            </div>
+            <textarea className="text_area" name="message"></textarea>
 
-          {/* Success Message Appears Here */}
-          {successMessage && (
-            <div className="success_message">{successMessage}</div>
-          )}
+            {/* Success Message Appears Here */}
+            {successMessage && (
+              <div className="success_message">{successMessage}</div>
+            )}
 
-          <div className="Submit_btn_div">
-            <button type="submit" className="Submit_btn">
-              Send
-            </button>
-          </div>
-        </form>
+            <div className="Submit_btn_div">
+              <button type="submit" className="Submit_btn">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
