5752c32e64363f7659fd598d16f523f29607c216
sazzad tomal <sazzadtomal1@gmail.com>
2025-07-18 05:29:51 +0600

    faqs page done

--- src/components/Faqs/Faqs.js
+++ src/components/Faqs/Faqs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import faqs_photo from "../../accets/faqs/faqs_photo.png";
 import "./Faqs.css";
 
 const Faqs = () => {
@@ -8,7 +9,7 @@ const Faqs = () => {
   document.documentElement.scrollTop = 0;
   return (
     <div className="faqs_parent_div">
-      <div className="faqs_customer_text_div">
+      {/* <div className="faqs_customer_text_div">
         <h2>Customers</h2>
         <div>
           <p>• How do I book a van? </p>
@@ -39,6 +40,66 @@ const Faqs = () => {
         <div>
           <p>• How do I get paid?</p>
           <p>Weekly bank deposits based on completed trips.</p>
+        </div>
+      </div> */}
+
+      <div className="faqs_grid_div">
+        <div className="faqs_question_div">
+          <div>
+            <h2>Customers</h2>
+            <div>
+              <select name="" id="">
+                <option value="">• How do I book a van?</option>
+                <option value="">Simply use our website.</option>
+              </select>
+
+              <br />
+
+              <select name="" id="">
+                <option value="">• How is pricing calculated?</option>
+                <option value="">Based on distance, time, and van size</option>
+              </select>
+
+              <br />
+
+              <select name="" id="">
+                <option value="">• What if my delivery is delayed?</option>
+                <option value="">We notify you and provide updates.</option>
+              </select>
+
+              <br />
+
+              <select name="" id="">
+                <option value="">• Do I need to help load/unload?</option>
+                <option value="">
+                  Our drivers assist, but extra manpower can be added.
+                </option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <h2>Drivers</h2>
+            <div>
+              <select name="" id="">
+                <option value="">• What documents do I need?</option>
+                <option value="">
+                  Driving license, insurance, vehicle registration.
+                </option>
+              </select>
+
+              <br />
+
+              <select name="" id="">
+                <option value="">• How do I get paid?</option>
+                <option value="">
+                  Weekly bank deposits based on completed trips.
+                </option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="faqs_photo_div">
+          <img src={faqs_photo} alt="" />
         </div>
       </div>
     </div>
