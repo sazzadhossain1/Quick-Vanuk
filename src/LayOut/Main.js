d56771d879b5b45fe16b709e1f2286cbb207bc63
sazzad tomal <sazzadtomal1@gmail.com>
2025-09-11 03:17:30 +0600

    what app add done

--- src/LayOut/Main.js
+++ src/LayOut/Main.js
@@ -2,6 +2,9 @@ import React, { useRef } from "react";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import "./Main.css";
 
 const Main = () => {
   const footerRef = useRef(null);
@@ -11,12 +14,21 @@ const Main = () => {
       footerRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
-
+  // 01884295409
   return (
-    <div>
+    <div className="main_parent_div">
       <NavigationBar scrollToFooter={scrollToFooter} />
       <Outlet />
       <Footer ref={footerRef} />
+
+      {/* WhatsApp Icon */}
+      <a
+        href="https://wa.me/447352622216" // replace with your WhatsApp number
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon className="whatsapp_icon" icon={faWhatsapp} />
+      </a>
     </div>
   );
 };
